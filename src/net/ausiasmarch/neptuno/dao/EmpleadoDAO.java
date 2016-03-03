df58b54cdada8fc9130b52cd1632e388b85a2c44
Dani <Dani@192.168.1.241>
2016-03-04 00:32:17 +0100

    falta implementar el buscado y algunas cosillas mas.

--- src/net/ausiasmarch/neptuno/dao/EmpleadoDAO.java
+++ src/net/ausiasmarch/neptuno/dao/EmpleadoDAO.java
@@ -3,11 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package net.ausiasmarch.neptuno.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import net.ausiasmarch.neptuno.entity.Empleado;
+import net.ausiasmarch.neptuno.model.DBConnection;
 
 /**
  *
@@ -15,9 +20,30 @@ import net.ausiasmarch.neptuno.entity.Empleado;
  */
 public class EmpleadoDAO implements GenericDAO<Empleado, Long> {
 
+    private final Connection conn = DBConnection.instance.getConnection();
+
     @Override
     public void create(Empleado t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        try {
+            String sentencia = "INSERT INTO empleado (nombre,apellidos,ciudad,idCargo,fechaNa,"
+                    + "fechaAlta,idOficina,,email)"
+                    + " VALUES ( ? , ? , ? , ? , ? , ? , ? , ? )";
+
+            PreparedStatement ps = conn.prepareStatement(sentencia);
+
+            ps.setString(1, t.getNombre());
+            ps.setString(2, t.getApellidos());
+            ps.setString(3, t.getCiudad());
+            ps.setInt(4, t.getIdCargo());
+            ps.setDate(5, t.getAlta());
+            ps.setDate(6, t.getNaci());
+            ps.setInt(7, t.getIdOficina());
+            ps.setString(8, t.getEmail());
+        } catch (SQLException ex) {
+            Logger.getLogger(EmpleadoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
     @Override
@@ -49,5 +75,5 @@ public class EmpleadoDAO implements GenericDAO<Empleado, Long> {
     public <K> Long newKey() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
--- src/net/ausiasmarch/neptuno/entity/Empleado.java
+++ src/net/ausiasmarch/neptuno/entity/Empleado.java
@@ -11,5 +11,95 @@ package net.ausiasmarch.neptuno.entity;
  * @author a048598270y
  */
 public class Empleado {
+    private long numEmple;
+    private String nombre,apellidos,ciudad,email,naci,alta;
+    private int idCargo,idOficina;
+    
+
+    public Empleado(long numEmple,String nombre,String apellidos,String ciudad,String email,int idCargo,int idOficina,String naci, String alta){
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.ciudad = ciudad;
+        this.email = email;
+        this.idOficina = idOficina;
+        this.idCargo = idCargo;
+        this.numEmple = numEmple;
+        this.alta = alta;
+        this.naci = naci;
+        
+    }
+
+    public String getNaci() {
+        return naci;
+    }
+
+    public void setNaci(String naci) {
+        this.naci = naci;
+    }
+
+    public String getAlta() {
+        return alta;
+    }
+
+    public void setAlta(String alta) {
+        this.alta = alta;
+    }
+    
+    public long getNumEmple() {
+        return numEmple;
+    }
+
+    public void setNumEmple(long numEmple) {
+        this.numEmple = numEmple;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdCargo() {
+        return idCargo;
+    }
+
+    public void setIdCargo(int idCargo) {
+        this.idCargo = idCargo;
+    }
+
+    public int getIdOficina() {
+        return idOficina;
+    }
+
+    public void setIdOficina(int idOficina) {
+        this.idOficina = idOficina;
+    }
+    
     
 }
